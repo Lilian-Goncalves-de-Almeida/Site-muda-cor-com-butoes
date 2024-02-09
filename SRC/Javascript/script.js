e68306b3e1ae3bc3c2905e7766f30727dfa574dd
Lilian Gonçalves de Almeida <59492113+Lilian-Goncalves-de-Almeida@users.noreply.github.com>
2024-02-08 21:46:08 -0300

    Initial commit

---
+++ SRC/Javascript/script.js
@@ -0,0 +1,24 @@
+function changeColorForBlue(){
+    document.body.style.setProperty('--color', '#46abb9');
+    document.body.style.setProperty('--second-color', '#2b6986');
+}
+
+function changeColorForPurple(){
+    document.body.style.setProperty('--color', '#6546b9');
+    document.body.style.setProperty('--second-color', '#391f63');
+}
+
+function changeColorForOrange(){
+    document.body.style.setProperty('--color', '#e68443');
+    document.body.style.setProperty('--second-color', '#772020');
+}
+
+function changeColorForYellow(){
+    document.body.style.setProperty('--color', '#ebe048');
+    document.body.style.setProperty('--second-color', '#ac861d');
+}
+
+function changeColorForRed(){
+    document.body.style.setProperty('--color', '#da4848');
+    document.body.style.setProperty('--second-color', '#772020');
+}
